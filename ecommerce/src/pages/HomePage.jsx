75c7e24d100b0431d6c2771935548f3f377a06e5
monikabaabu <monikababu207@gmail.com>
2026-06-13 07:01:42 +0530

    generated html for the cart items

--- ecommerce/src/pages/HomePage.jsx
+++ ecommerce/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import "./HomePage.css";
 import { useEffect,useState } from "react";
 import { Header } from "../components/Header";
 import CheckmarkIcon from "../assets/images/icons/checkmark.png";
+import { formatMoney } from "../utils/money";
 export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
   
@@ -41,7 +42,7 @@ export function HomePage({ cart }) {
                 </div>
 
                 <div className="product-price">
-                  ${(product.priceCents / 100).toFixed(2)}
+                  {formatMoney(product.priceCents)}
                 </div>
 
                 <div className="product-quantity-container">
