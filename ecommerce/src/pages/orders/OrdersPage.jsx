0a3ab8fa106088deb5d9e8f97806502c4324e5e2
monikabaabu <monikababu207@gmail.com>
2026-06-15 20:03:48 +0530

    add loadCart functionality to orders pages and components

--- ecommerce/src/pages/orders/OrdersPage.jsx
+++ ecommerce/src/pages/orders/OrdersPage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 import "./OrdersPage.css";
 import { Header } from "../../components/Header";
 import { OrdersGrid } from "./OrdersGrid";
-export function OrdersPage({ cart }) {
+export function OrdersPage({ cart , loadCart }) {
   const [orders, setOrders] = useState([]);
   useEffect(() => {
     const fetchOrdersData = async () => {
@@ -22,7 +22,7 @@ export function OrdersPage({ cart }) {
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
 
-        <OrdersGrid orders={orders} />
+        <OrdersGrid orders={orders} loadCart={loadCart} />
       </div>
     </>
   );
