44b99391106cc5fd0ed51dfec2a81d123679b8bb
monikabaabu <monikababu207@gmail.com>
2026-06-15 19:33:23 +0530

    create update delivery option feature

--- ecommerce/src/pages/checkout/OrderSummary.jsx
+++ ecommerce/src/pages/checkout/OrderSummary.jsx
@@ -2,17 +2,18 @@
 import {CartItemDetails} from './CartItemDetails';
 import { DeliveryOptions } from "./DeliveryOptions";
 import { DeliveryDate } from "./DeliveryDate";
-export function OrderSummary({cart, deliveryOptions}) {
+export function OrderSummary({cart, deliveryOptions, loadCart}) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 && cart.map((cartItem) => {
           
           return (
             <div key={cartItem.productId} className="cart-item-container">
-              <DeliveryDate cartItem={cartItem} deliveryOptions={deliveryOptions} />
+              <DeliveryDate cartItem={cartItem} deliveryOptions={deliveryOptions}  />
               <div className="cart-item-details-grid">
 
                 <CartItemDetails cartItem={cartItem} />
+                <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} loadCart={loadCart} />
               </div>
             </div>
           );
