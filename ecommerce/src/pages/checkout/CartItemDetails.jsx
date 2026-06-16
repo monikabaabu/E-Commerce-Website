291df51d25ba1453ca4ff0ddaf3d53c6cd242986
monikabaabu <monikababu207@gmail.com>
2026-06-16 15:08:54 +0530

    implement quantity update functionality in cart item details and add styling for quantity input

--- ecommerce/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce/src/pages/checkout/CartItemDetails.jsx
@@ -1,9 +1,19 @@
 import {formatMoney} from "../../utils/money";
 import axios from "axios";
+import {useState} from "react";
 export function CartItemDetails({cartItem , loadCart}) {
+  const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
+  };
+  const updateQuantity =  () => {
+    if (isUpdatingQuantity) {
+      setIsUpdatingQuantity(false);
+    }
+    else {
+      setIsUpdatingQuantity(true);
+    }
   };
   return (
     <>
@@ -15,12 +25,14 @@ export function CartItemDetails({cartItem , loadCart}) {
       </div>
       <div className="product-quantity">
         <span>
-          Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+          Quantity: {isUpdatingQuantity ? <input type="text" className="quantity-textbox" />
+          :<span className="quantity-label">{cartItem.quantity}</span>
+        }
         </span>
-        <span className="update-quantity-link link-primary">Update</span>
-        <span className="delete-quantity-link link-primary" onClick={deleteCartItem}>Delete</span>
+        <span className="update-quantity-link link-primary" onClick={updateQuantity}>Update</span>
+        <span className="delete-quantity-link link-primary"  onClick={deleteCartItem}>Delete</span>
       </div>
     </div>
     </>
-  );
+  ); 
 }
