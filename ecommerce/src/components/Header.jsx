020c4abe49674283bb91dea98e449d31fa9592ca
monikabaabu <monikababu207@gmail.com>
2026-06-16 18:43:36 +0530

    create unit tests for format money

--- ecommerce/src/components/Header.jsx
+++ ecommerce/src/components/Header.jsx
@@ -1,10 +1,22 @@
-import {NavLink} from 'react-router';
+import {NavLink, useNavigate , useSearchParams} from 'react-router';
 import CartIcon from '../assets/images/icons/cart-icon.png' ;
 import SearchIcon from '../assets/images/icons/search-icon.png' ;
 import LogoWhite from '../assets/images/logo-white.png' ;
 import MobileLogoWhite from '../assets/images/mobile-logo-white.png' ;
+import { useState } from 'react';
 import './Header.css' ;
 export function Header({cart}){
+    const navigate = useNavigate();
+    
+    const [searchParams] = useSearchParams();
+    const searchText = searchParams.get("search");
+    const [search, setSearch] = useState(searchText || '');
+    const updateSearchInput = (event) => {
+        setSearch(event.target.value);
+    };
+    const searchProducts = () => {
+        navigate(`/?search=${search}`);
+    };
     let totalQuantity = 0;
     cart.forEach((cartItem) => {
         totalQuantity += cartItem.quantity;
@@ -21,9 +33,9 @@ export function Header({cart}){
             </div>
 
             <div className="middle-section">
-                <input className="search-bar" type="text" placeholder="Search" />
+                <input className="search-bar" type="text" placeholder="Search" value={search} onChange={updateSearchInput} />
 
-                <button className="search-button">
+                <button className="search-button" onClick={searchProducts}>
                 <img className="search-icon" src={SearchIcon} />
                 </button>
             </div>
--- ecommerce/src/pages/home/HomePage.jsx
+++ ecommerce/src/pages/home/HomePage.jsx
@@ -3,17 +3,21 @@ import "./HomePage.css";
 import { useEffect,useState } from "react";
 import { Header } from "../../components/Header";
 import { ProductsGrid } from "./ProductsGrid";
+import {useSearchParams} from "react-router";
 export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
-  
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get("search");
+
   useEffect( () => {
     const getHomeData = async () => {
-      const response = await axios.get("/api/products");
+      const urlPath = search? `/api/products?search=${search}` : '/api/products';
+      const response = await axios.get(urlPath);
       setProducts(response.data);
     };
     getHomeData();
     
-}, []);
+}, [search]);
 
   return (
     <>
